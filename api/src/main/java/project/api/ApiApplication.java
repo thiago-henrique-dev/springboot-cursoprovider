e54475903a719575f193af850400f5a4ccbaa625
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-05-19 11:44:05 -0300

    first commit

---
+++ api/src/main/java/project/api/ApiApplication.java
@@ -0,0 +1,22 @@
+package project.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "hello world";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
