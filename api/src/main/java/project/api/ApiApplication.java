9dea36622cdc0a0a40e1ab5488ef5174018c6317
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-05-20 22:00:37 -0300

    create table and class model

--- api/src/main/java/project/api/ApiApplication.java
+++ api/src/main/java/project/api/ApiApplication.java
@@ -1,29 +1,13 @@
 package project.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
-@RestController
 public class ApiApplication {
 
-	@Value("${application.name}")
-	private String applicationName;
-
-	@GetMapping("/hello")
-	public String helloWorld(){
-		return applicationName;
-	}
-
-
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
